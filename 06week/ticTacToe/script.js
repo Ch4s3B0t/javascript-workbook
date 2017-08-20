694433b762a28f19c16953fb79229a8f25f69643
Chase Turner <chasethemigthy1@gmail.com>
2017-08-20 14:25:12 -0500

    Commit to save data from Atom crash

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -2,4 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  //Start with blank board
+  //on click, check for empty space, if yes then apply x or o(event listener on data cells)
+  //(how to get data cell attributes)
+  //rotate player to switch between x and o
+  //at the end of each turn, test for a win.
+  //a win would be x or o matching horizontal, diagonal, or vertical
+//put data cell attributes into an array
+  /*
+
+  */
 });
